4f22e80c3db147afe806de27405fec6203eae68b
Łukasz <lukasz.f.d@gmail.com>
2024-10-17 21:53:00 +0200

    update

--- tests/api/src/main/java/com/my/spring/boot/api/ApiApplication.java
+++ tests/api/src/main/java/com/my/spring/boot/api/ApiApplication.java
@@ -1,13 +1,12 @@
-package com.example.api;
+package com.my.spring.boot.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
 }
