b3b321486da623a82677eb898fb8b674cc14a29e
Łukasz <lukasz.f.d@gmail.com>
2024-10-18 17:52:56 +0200

    nikto

---
+++ tests/2. nikto/script.sh
@@ -0,0 +1,6 @@
+# /home/kali/test_api/
+# -h: This flag specifies the target host that Nikto will scan
+# -o: This flag indicates that you want to output the results of the scan to a file
+# wynik_nikto.txt: This is the name of the output file where Nikto will save the results of the scan. You can choose any name you prefer for the output file
+
+nikto -h http://localhost:8080 -o wynik_nikto.txt
