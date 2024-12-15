57be6d6047a0473ad75f518600c80463cb745b41
Łukasz <lukasz.f.d@gmail.com>
2024-12-15 18:05:04 +0100

    new tests ( tshark, hping3)

--- tests/4. wireshark/cmd.bat
+++ tests/4. wireshark/cmd.bat
@@ -1,3 +1,8 @@
 tshark.exe -r "C:\Users\c50-AfricanFalls3\UNODC-GPC-001-003-JohnDoe-NetworkCapture-2021-04-29.pcapng" -Y "ftp && ip.src == 192.168.1.26"
 
 tshark.exe -r "C:\Users\c50-AfricanFalls3\UNODC-GPC-001-003-JohnDoe-NetworkCapture-2021-04-29.pcapng" -Y "ftp" -T fields -e data | findstr "Pass"
+
+tshark.exe -i Wi-Fi -Y "ip.src == 192.168.1.00 or ip.src == 192.168.1.00 or ip.dst == 192.168.1.00 or ip.dst == 192.168.1.00"
+
+tshark.exe -i 5 -Y "ip.src == 192.168.1.00 or ip.src == 192.168.1.00 or ip.dst == 192.168.1.00 or ip.dst == 192.168.1.00"
+
